6c58ded923aa3994a45a877d9c45a04de54c9bf8
rahulnene <rahul.nene@live.com>
2018-07-24 14:36:25 +0530

    --

---
+++ testModule.py
@@ -0,0 +1,12 @@
+class Point:
+    def __init__(self, xCoord, yCoord, zCoord):
+        self.xCoord = xCoord
+        self.yCoord = yCoord
+        self.zCoord - zCoord
+
+#ax + by = c
+class Line:
+    def __init__(self, a, b, c):
+        self.slope = -a/b
+        self.intercept = c/a
+
