aa800752451cbf2f6ead99a491496fca8b91d465
Rahul nene <rahul.nene@live.com>
2020-10-13 18:43:33 +0530

    asdf

--- main.py
+++ main.py
@@ -1,14 +1,29 @@
 from time import perf_counter
-from primefactors import factorize
-
-# from math import log2, sqrt, floor, ceil, gcd
+# from primefactors import factorize
+from math import sqrt, factorial, log10, floor
 
 t0 = perf_counter()
 
+def checkBounce(n):
+    nStr = str(n)
+    nList = []
+    for digit in nStr:
+        nList.append(int(digit))
+    nsorted = sorted(nList)
+    if nList == nsorted or nList == nsorted[::-1]:
+        return False
+    return True
 
 
+ans = 0
+for i in range(1,10000000000):
+    bouncy = checkBounce(i)
+    if not bouncy:
+        ans += 1
 
+print(ans)
 
+# print(checkBounce(132))
 
 tf = perf_counter() - t0
 print(tf, "seconds")
