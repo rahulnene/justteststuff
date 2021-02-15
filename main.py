1e0b3a3873f0976ec4bd78b3223b8465ff899712
Rahul nene <rahul.nene@live.com>
2021-02-15 09:18:42 +0530

    asdf

--- main.py
+++ main.py
@@ -1,29 +1,46 @@
 from time import perf_counter
-# from primefactors import factorize
-from math import sqrt, factorial, log10, floor
+from sympy import *
+from math import pi
 
 t0 = perf_counter()
+x, y = symbols('x,y')
 
-def checkBounce(n):
-    nStr = str(n)
-    nList = []
-    for digit in nStr:
-        nList.append(int(digit))
-    nsorted = sorted(nList)
-    if nList == nsorted or nList == nsorted[::-1]:
-        return False
-    return True
+a, b = 0,1
+n = 10
+h = (b - a) / n
+funcy = 16*(x-1)/(x**4 - 2*x**3 + 4*x - 4)
 
 
-ans = 0
-for i in range(1,10000000000):
-    bouncy = checkBounce(i)
-    if not bouncy:
-        ans += 1
+def y(x0):
+    return funcy.subs(x, x0)
 
-print(ans)
 
-# print(checkBounce(132))
+xarr = set()
+for i in [x / (1 / h) for x in range(ceiling(a / h), ceiling(b / h + h))]:
+    xarr.add(i)
+xarr.add(a)
+xarr.add(b)
+xarr = sorted(list(xarr))
+yarr = []
 
-tf = perf_counter() - t0
-print(tf, "seconds")
+for i in range(0, n + 1):
+    yarr.append(y(xarr[i]))
+
+
+def integrate():
+    print(xarr)
+    print(yarr)
+    ans = yarr[0] + yarr[n]
+    for odd in range(1, n, 2):
+        ans += 4 * yarr[odd]
+    for even in range(2, n - 1, 2):
+        ans += 2 * yarr[even]
+    ans *= h / 3
+    return ans
+
+
+print(integrate())
+print(pi)
+
+tf = perf_counter()
+print(tf - t0, "seconds")
